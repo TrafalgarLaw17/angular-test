1fd2839bf18cf62807a7b48d015731efe414e8da
TrafalgarLaw17 <jamiljohnandrada@gmail.com>
2025-03-20 20:28:00 +0800

    crud and permissions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router, NavigationEnd } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
